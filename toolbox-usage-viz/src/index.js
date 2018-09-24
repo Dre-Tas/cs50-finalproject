89ae1e48a52252175a7b2a9f3ae5a09259c873a9
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-24 18:00:27 +1000

    flask server now showing the react app

--- toolbox-usage-viz/src/index.js
+++ toolbox-usage-viz/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log("Hello!");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
