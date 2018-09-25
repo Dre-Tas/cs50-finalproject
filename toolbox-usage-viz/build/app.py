f45eabc6e9a3fe54cb6d908f41d2c5145b5a3727
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-25 17:32:44 +1000

    server now able to send test data to fron-end - react able to read it and display it

--- toolbox-usage-viz/build/app.py
+++ toolbox-usage-viz/build/app.py
@@ -1,9 +1,26 @@
 import os
 import re
 from flask import Flask, jsonify, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
+engine = create_engine(
+    'sqlite:///C:\\CS50\\cs50-finalproject\\toolboxrecords-poc.db')
+db = scoped_session(sessionmaker(bind=engine))
+
 
 @app.route('/')
 def index():
+    # return render_template('index.html', rows_count=len(rows))
     return render_template('index.html')
+
+
+@app.route('/countrow')
+def countrow():
+    rows = db.execute("SELECT * FROM records").fetchall()
+    countrow = len(rows)
+    return jsonify({"data": "Hello! This string comes from the server ;)"})
--- toolbox-usage-viz/src/App.js
+++ toolbox-usage-viz/src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {data: null}
+        this.getData();
+    }
+
+    getData() {
+        let data = fetch('http://127.0.0.1:5000/countrow')
+            .then((resp) => {
+                resp.json().then((res) => {
+                    console.log(res.data);
+                    this.setState({data: res.data});
+                });
+            })
+    }
+
+    render() {
+        return (<div className="App">
+            <h1>{this.state.data}</h1>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <p className="App-intro">
+                To get started, edit
+                <code>src/App.js</code>
+                and save to reload.
+            </p>
+        </div>);
+    }
 }
 
 export default App;
