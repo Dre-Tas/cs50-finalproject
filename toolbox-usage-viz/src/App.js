883e0701d794f9d8464d48f1ca94386d15a5b786
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-26 17:38:07 +1000

    react app taking list of json

--- toolbox-usage-viz/src/App.js
+++ toolbox-usage-viz/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
     constructor() {
         super();
-        this.state = {data: null}
+        this.state = {}
         this.getData();
     }
 
@@ -13,15 +13,14 @@ class App extends Component {
         let data = fetch('http://127.0.0.1:5000/countrow')
             .then((resp) => {
                 resp.json().then((res) => {
-                    console.log(res.data);
-                    this.setState({data: res.data});
+                    this.setState(res);
                 });
             })
     }
 
     render() {
         return (<div className="App">
-            <h1>{this.state.data}</h1>
+            <h1>{this.state.tool}</h1>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <h1 className="App-title">Welcome to React</h1>
