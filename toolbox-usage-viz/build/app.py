d6bec8b1a3a41464d09c4a9e3beef1b7b18bbe49
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-26 17:17:42 +1000

    parse result of query to json in endpoint

--- toolbox-usage-viz/build/app.py
+++ toolbox-usage-viz/build/app.py
@@ -1,26 +1,43 @@
-import os
-import re
 from flask import Flask, jsonify, render_template, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_cors import CORS, cross_origin
 
+# Set up flask app with CORS
 app = Flask(__name__)
 CORS(app)
 
-engine = create_engine(
-    'sqlite:///C:\\CS50\\cs50-finalproject\\toolboxrecords-poc.db')
-db = scoped_session(sessionmaker(bind=engine))
+# Set up SQLAlchemy + Marshmellow
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\CS50\\cs50-finalproject\\toolboxrecords-poc.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
+
+# Create db model class
+class Record(db.Model):
+    __tablename__ = "records"
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String)
+    tool = db.Column(db.String)
+    revitversion = db.Column(db.String)
+    start = db.Column(db.DateTime)
+    end = db.Column(db.DateTime)
+    size = db.Column(db.Integer)
+
+# Create marshmallow schema
+class RecordSchema(ma.ModelSchema):
+    class Meta:
+        model = Record
 
 @app.route('/')
 def index():
-    # return render_template('index.html', rows_count=len(rows))
     return render_template('index.html')
 
-
+# endpoint
 @app.route('/countrow')
 def countrow():
-    rows = db.execute("SELECT * FROM records").fetchall()
-    countrow = len(rows)
-    return jsonify({"data": "Hello! This string comes from the server ;)"})
+    records = Record.query.all()
+    records_schema = RecordSchema(many=True)
+    output = records_schema.dump(records).data
+    return jsonify(output)
