04de3c7035138e9bb27dea7100776aab01b4b85f
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-27 15:40:30 +1000

    app showing how many times + for how long

--- toolbox-usage-viz/src/App.js
+++ toolbox-usage-viz/src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import moment from 'moment';
+import _ from 'lodash';
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            records_lst : [],
+        }
         this.getData();
     }
 
@@ -13,23 +17,27 @@ class App extends Component {
         let data = fetch('http://127.0.0.1:5000/api/dbrecords')
             .then((resp) => {
                 resp.json().then((res) => {
-                    this.setState(res);
+                    this.setState({records_lst : res});
                 });
             })
     }
 
     render() {
+        let total_usage_time = 0;
         return (<div className="App">
-            <h1>{this.state.tool}</h1>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">
-                To get started, edit
-                <code>src/App.js</code>
-                and save to reload.
-            </p>
+            {this.state.records_lst.map(function(lst, i) {
+                let start = new Date(Date.parse(lst.start));
+                let end = new Date(Date.parse(lst.end));
+                total_usage_time += end - start
+                // return <p key={i}>{moment(start).format("DD/MM/YYYY")}</p>
+            })}
+            <h2>Willow.Design() has been used {this.state.records_lst.length} times!</h2>
+            <br></br>
+            <h1>For a total run time of {total_usage_time} milliseconds...</h1>
+            <h2>or {total_usage_time / 1000} seconds...</h2>
+            <h3>or {total_usage_time / 1000 / 60} minutes...</h3>
+            <h4>or {total_usage_time / 1000 / 60 / 60} hours...</h4>
+            <h5>or {total_usage_time / 1000 / 60 / 60 / 60} days...</h5>
         </div>);
     }
 }
