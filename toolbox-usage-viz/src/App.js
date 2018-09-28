089a9e31cabbe27d63ba706c3f8339234b263546
Andrea Tassera <36023527+Dre-Tas@users.noreply.github.com>
2018-09-28 11:25:18 +1000

    app broken into classes called by App - better structure

--- toolbox-usage-viz/src/App.js
+++ toolbox-usage-viz/src/App.js
@@ -23,28 +23,20 @@ class App extends Component {
             })
     }
 
-    calcTotalUsageTime() {
-        // Calculates the time in milliseconds
-        let total_usage_time = 0;
 
-        {this.state.records_lst.map(function(lst, i) {
-            let start = new Date(Date.parse(lst.start));
-            let end = new Date(Date.parse(lst.end));
-            total_usage_time += end - start
-        })};
-
-        return total_usage_time / 1000;
-    }
 
     render() {
-        var usage_time = this.calcTotalUsageTime()
-
-        var versions = new Set([]);
+        // var usage_time = this.calcTotalUsageTime()
+        //
+        // var versions = new Set([]);
 
         return (<div className="App">
-            <h2>Willow.Design() has been used {this.state.records_lst.length} times!</h2>
-            <br></br>
-            <h1>For a total run time of {usage_time.toFixed(2)} seconds...</h1>
+
+          <TimesUsed pass={this.state.records_lst}/>
+
+          <TotUsageTime pass={this.state.records_lst}/>
+
+            {/* <h1>For a total run time of {usage_time.toFixed(2)} seconds...</h1>
             <h3>or {(usage_time / 60 / 60).toFixed(2)} hours...</h3>
 
             {this.state.records_lst.map(function(lst, i) {
@@ -53,43 +45,86 @@ class App extends Component {
 
             {Array.from(versions).map(function(lst, i) {
                 return <p key={i}>{lst}</p>
-            })}
+            })} */}
 
             {/* <HBarGraph records={records}/> */}
         </div>);
     }
 }
 
-export default App;
-
-class HorBarG extends Component {
-    constructor (props) {
+class TimesUsed extends Component {
+    constructor(props) {
         super(props);
-        this.state = {
-            a : []
-        };
     }
 
-
+    render() {
+        return (
+            <h2>Willow.Design() has been used {this.props.pass.length} times!</h2>
+        );
+    }
 }
 
-const HBarGraph = (props) => {
-    const data = {
-      labels: ['2018', '2017'],
-      datasets: [
-        {
-          label: 'Which version is more popular?',
-          backgroundColor: 'rgba(255,153,0,0.2)',
-          borderColor: 'rgba(255,153,0,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [65, 59]
-        }
-      ]
-    };
+class TotUsageTime extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-    return (
-        <HorizontalBar data={data}/>
-    )
+    calcTotalUsageTime() {
+    // Calculates the time in milliseconds
+    let total_usage_time = 0;
+
+    {this.props.pass.map(function(lst, i) {
+        let start = new Date(Date.parse(lst.start));
+        let end = new Date(Date.parse(lst.end));
+        total_usage_time += end - start
+    })};
+
+    return total_usage_time / 1000;
+    }
+
+    render() {
+        var usage_time = this.calcTotalUsageTime();
+
+        return(
+            <div>
+                <h1>For a total run time of {usage_time.toFixed(2)} seconds...</h1>
+                <h3>or {(usage_time / 60 / 60).toFixed(2)} hours...</h3>
+            </div>
+        )
+    }
 }
+
+
+export default App;
+
+// class HorBarG extends Component {
+//     constructor (props) {
+//         super(props);
+//         this.state = {
+//             a : []
+//         };
+//     }
+//
+//
+// }
+//
+// const HBarGraph = (props) => {
+//     const data = {
+//       labels: ['2018', '2017'],
+//       datasets: [
+//         {
+//           label: 'Which version is more popular?',
+//           backgroundColor: 'rgba(255,153,0,0.2)',
+//           borderColor: 'rgba(255,153,0,1)',
+//           borderWidth: 1,
+//           hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+//           hoverBorderColor: 'rgba(255,99,132,1)',
+//           data: [65, 59]
+//         }
+//       ]
+//     };
+//
+//     return (
+//         <HorizontalBar data={data}/>
+//     )
+// }
